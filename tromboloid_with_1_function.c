c0d95002b41737f679d19bd3c991f19153fe6243
ENG19CS0363-VISHNUNAIR <76106992+ENG19CS0363-VISHNUNAIR@users.noreply.github.com>
2021-02-05 19:50:52 +0530

    Update tromboloid_with_1_function.c

--- tromboloid_with_1_function.c
+++ tromboloid_with_1_function.c
@@ -9,7 +9,7 @@ printf("enter value of d:");
 scanf("%f",&d);
 printf("enter value of b:");
 scanf("%f",&b);
-volume= 1/3((h*d*b)+(d/b));
+volume= 1/3*(h*d*b)+(d/b);
 printf("volume of tromboloid is:%f",volume);
 return 0;
 }
