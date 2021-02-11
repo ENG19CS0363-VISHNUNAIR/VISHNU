a203ade42aa4f75e844c412ed167cae74f3c8e3c
ENG19CS0363-VISHNUNAIR <76106992+ENG19CS0363-VISHNUNAIR@users.noreply.github.com>
2021-02-11 16:20:11 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -12,13 +12,13 @@ float distance(float x1,float x2,float y1,float y2)
 float dist=sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 return dist;
 }
-void output(float x1,float x2,float y1,float y2)
+void output(float dist)
 {
 printf("the distance between two points is :%f",dist);
 }
 int main()
 {
-float x1,float x2,float y1,float y2;
+float x1,x2,y1,y2;
 printf("enter x1:");
 x1=input();
 printf("enter x2:");
@@ -27,7 +27,7 @@ printf("enter y1:");
 y1=input();
 printf("enter y2:");
 y2=input();
-distance=sqrt(dist);
-output(x1,x2,y1,y2,distance);
+int d=distance(x1,x2,y1,y2);
+output(d);
 return 0;
 }
